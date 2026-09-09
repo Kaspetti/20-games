1ef3ea0420ebb86a180574d85436c790f4f83f5e
Kaspar.Moberg <kaspar.moberg@uib.no>
2026-09-09 09:53:42 +0200

    implemented movement system

--- breakout/src/breakout.rs
+++ breakout/src/breakout.rs
@@ -1,7 +1,15 @@
-use crate::bar::BarPlugin;
 use crate::state::StatePlugin;
+use crate::{bar::BarPlugin, movement::MovementPlugin};
 
 use bevy::{camera::ScalingMode, prelude::*};
+
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum MovementSet {
+    Input,
+    Movement,
+    Bounds,
+    Collision,
+}
 
 pub struct BreakoutPlugin;
 
@@ -9,6 +17,19 @@ impl Plugin for BreakoutPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(StatePlugin);
         app.add_plugins(BarPlugin);
+
+        app.configure_sets(
+            Update,
+            (
+                MovementSet::Input,
+                MovementSet::Movement,
+                MovementSet::Bounds,
+                MovementSet::Collision,
+            )
+                .chain(),
+        );
+
+        app.add_plugins(MovementPlugin);
 
         app.add_systems(Startup, setup);
     }
--- breakout/src/movement.rs
+++ breakout/src/movement.rs
@@ -1,13 +1,23 @@
 use bevy::prelude::*;
 
+use crate::breakout::MovementSet;
+
 pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, movement_system.in_set(MovementSet::Movement));
+    }
 }
 
 #[derive(Component)]
 struct Movement {
     speed: f32,
     direction: Vec3,
+}
+
+fn movement_system(mut query: Query<(&mut Transform, &Movement)>, time: Res<Time>) {
+    for (mut transform, movement) in &mut query {
+        transform.translation += movement.direction * movement.speed * time.delta_secs();
+    }
 }
